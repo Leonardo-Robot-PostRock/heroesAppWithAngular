2ecbae729d519838645d5c9d13c7a22659f47e2e
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-02 01:12:19 -0300

    Put router module in exports

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -6,10 +6,12 @@ import { AddComponent } from './pages/add/add.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { ListComponent } from './pages/list/list.component';
 import { SearchComponent } from './pages/search/search.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'list',
@@ -43,6 +45,9 @@ const routes: Routes = [
   declarations: [],
   imports: [
     RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class HeroesRoutingModule {}
