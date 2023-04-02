a251b71f1da834baaad6c2124f4ad1f24b703710
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-02 00:06:38 -0300

    loading child routes with loadChildren

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
     path: '404',
     component: ErrorPageComponent
   },
   {
     path: '**',
     redirectTo: '404'
-  },
+  }
 ];
 
 @NgModule({
