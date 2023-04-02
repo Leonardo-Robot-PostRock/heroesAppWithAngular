38737b6cea14e83afa4ab533987588c143266965
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-02 00:17:37 -0300

    put the AuthRouting module in imports

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
@@ -11,7 +12,8 @@ import { RegistrationComponent } from './pages/registration/registration.compone
     RegistrationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
