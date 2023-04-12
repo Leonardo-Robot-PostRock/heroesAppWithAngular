33fb69d2246b9cac3d47ecc7823dc28966fa13a7
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-12 20:42:55 -0300

    the id is printed by console

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {switchMap} from 'rxjs';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class HeroeComponent {
-
+export class HeroeComponent implements OnInit {
+  heroeId?: string;
+  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService) {}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({id})=> console.log(id))
+  }
 }
