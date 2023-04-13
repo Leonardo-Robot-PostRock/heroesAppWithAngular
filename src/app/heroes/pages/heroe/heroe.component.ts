2b5c7cc7ed644a0b0a1472a896abcf8436e9db6f
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-13 00:55:54 -0300

    creation of the method goBack and styles are added

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,23 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { delay, switchMap, tap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+
 import { HeroesService } from '../../services/heroes.service';
 import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: []
+  styles: [
+    `
+      .main-spinner {
+        height: calc(100vh - 84px);
+        justify-content: center;
+      }
+      
+      .container-spinner {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        min-width: 280px;
+      }
+
+      img {
+        width: 45%;
+        border-radius: 5px;
+      }
+    `
+  ]
 })
 export class HeroeComponent implements OnInit {
   heroe!: Heroe;
 
-
-  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService) {}
+  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService, private router: Router) {}
   ngOnInit(): void {
-    this.activatedRoute.params.pipe(delay(2000)).pipe(switchMap(({ id }) => this.heroesService.getHeroesById(id)),
-    ).subscribe((heroe)=>{
-      this.heroe = heroe;
-    })
+    this.activatedRoute.params
+      .pipe(delay(2000))
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroesById(id)))
+      .subscribe((heroe) => {
+        this.heroe = heroe;
+      });
+  }
+
+  goBack() {
+    this.router.navigate(['/heroes/list']);
   }
 }
