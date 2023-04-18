55a04b6dd22398e50767273bbc7644a6ec03c7e0
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-18 04:32:08 -0300

    use of environments

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 import { Observable } from 'rxjs';
+import { environments } from 'src/environments/environments';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl;
+
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroesById(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 
   getSuggestions(query: string): Observable<Heroe[]> {
