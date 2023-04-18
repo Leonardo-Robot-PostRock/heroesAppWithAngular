f1f0e14ed2f39304c5d757d4d45a9acd205e53ec
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-18 03:15:46 -0300

    the routes are modified

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,17 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 //Components
 import { LoginComponent } from './pages/login/login.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: LayoutPageComponent,
     children: [
       {
         path: 'login',
         component: LoginComponent
       },
       {
-        path: 'register',
+        path: 'new-account',
         component: RegistrationComponent
       },
       {
