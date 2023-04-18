ececd6c7c045cf70fc218ab056903490f2e96557
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-18 01:45:56 -0300

    the method getSuggestions is created

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -14,6 +14,10 @@ export class HeroesService {
   }
 
   getHeroesById(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`);
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+  }
+
+  getSuggestions(query: string): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`heroes?q=${query}&_limit=6`);
   }
 }
