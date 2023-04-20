e2b77f41a3456951dca78d327437a579ea68e8a2
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-19 23:53:56 -0300

    word correction

--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
-import { Heroe } from '../../interfaces/heroes.interface';
+import { Hero } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-list',
@@ -19,9 +19,9 @@ import { Heroe } from '../../interfaces/heroes.interface';
 export class ListComponent implements OnInit {
   constructor(private heroesService: HeroesService) {}
 
-  public heroes: Heroe[] = [];
+  public heroes: Hero[] = [];
 
   ngOnInit(): void {
-    this.heroesService.getHeroes().subscribe((heroes: Heroe[]) => (this.heroes = heroes));
+    this.heroesService.getHeroes().subscribe((heroes: Hero[]) => (this.heroes = heroes));
   }
 }
