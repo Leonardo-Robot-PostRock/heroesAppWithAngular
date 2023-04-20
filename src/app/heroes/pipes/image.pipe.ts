e5b87504614206a1743ca5fe4e25b2886a405bfc
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-19 23:53:05 -0300

    improved pipe

--- src/app/heroes/pipes/image.pipe.ts
+++ src/app/heroes/pipes/image.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Heroe } from '../interfaces/heroes.interface';
+import { Hero } from '../interfaces/heroes.interface';
 
 @Pipe({
   name: 'image'
 })
 export class ImagePipe implements PipeTransform {
-  transform(heroe: Heroe): string {
+  transform(heroe: Hero): string {
     if (!heroe.id && !heroe.alt_img) {
       return 'assets/no-image.png';
     }
