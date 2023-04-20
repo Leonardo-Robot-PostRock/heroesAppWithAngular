7e601048b02d1cd4302182567a8bee2e2b0baa9e
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-20 16:07:05 -0300

    publisher object is created

--- src/app/heroes/pages/add/add.component.ts
+++ src/app/heroes/pages/add/add.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class AddComponent {
-
+  public publishers = [
+    {
+      id: 'DC Comics',
+      value: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      value: 'Marvel - Comics'
+    }
+  ];
 }
