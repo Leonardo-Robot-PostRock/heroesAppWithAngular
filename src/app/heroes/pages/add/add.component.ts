aa64f6de27a6c8296d0ebe9796a455eabf43988f
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-27 02:19:17 -0300

    se establece el valor del formularioen ngOnInit y se actualiza y crean heroes en el metodo onSubmit

--- src/app/heroes/pages/add/add.component.ts
+++ src/app/heroes/pages/add/add.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { HeroesService } from '../../services/heroes.service';
 
 import { Publisher } from '../../interfaces/heroes.interface';
 import { Hero } from '../../interfaces/heroes.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styles: []
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
     superhero: new FormControl<string>('', { nonNullable: true }),
@@ -32,16 +35,36 @@ export class AddComponent {
     }
   ];
 
-  constructor(private heroesService: HeroesService) {}
+  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute, private router: Router) {}
 
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
     return hero;
   }
 
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params.pipe(switchMap(({ id }) => this.heroesService.getHeroesById(id)),).subscribe((hero)=>{
+      if(!hero) return this.router.navigate(['/']);
+      
+      this.heroForm.reset(hero)
+      return;
+    })
+  }
+
   onSubmit(): void {
     if (this.heroForm.invalid) return;
 
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero).subscribe((hero) => {
+        //TODO mostrar snackbar
+      });
+      return;
+    }
+    this.heroesService.addHero(this.currentHero).subscribe((hero) => {
+      //TODO mostrar snackbar, y navegar a /heroes/edit/hero.id
+    });
     // this.heroesService.updateHero(this.heroForm.value);
   }
 }
