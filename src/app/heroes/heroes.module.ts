ef00f03506b81e800d0c30098e307735a5ce2629
Leonardo <leonardo38361@gmail.com>
2023-11-08 05:09:27 -0300

    Se agrega nuevo componente a modulo de heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,13 +7,14 @@ import { SearchComponent } from './pages/search/search.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+import { HeroesCardComponent } from './components/heroes-card/heroes-card.component';
+import { ConfirDialogComponent } from './components/confir-dialog/confir-dialog.component';
 
 //Heroes routing module
 import { HeroesRoutingModule } from './heroes-routing.module';
 
 //Material Module
 import { MaterialModule } from '../material/material.module';
-import { HeroesCardComponent } from './components/heroes-card/heroes-card.component';
 import { ImagePipe } from './pipes/image.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -26,6 +27,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     ListComponent,
     SearchComponent,
     ImagePipe,
+    ConfirDialogComponent,
   ],
   imports: [
     CommonModule,
